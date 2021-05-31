08e22d69e318cddfaf4a10d274cadba1a1f37730
Kyle <kyle216@gmail.com>
2021-05-30 20:36:48 -0700

    Implemented multiples of 100 not being leap years
    Now 1900 returns False, as it is not a leap year according to the implementation. The 2nd test asserts that it is False now and passes.

--- leapyear.py
+++ leapyear.py
@@ -4,9 +4,15 @@ def leapCheck(int):
         print(int + " is not a leap year")
         return False
     else:
-        int = str(int)
-        print(int + " is a leap year")
-        return True
+        if int % 100 != 0:
+            int = str(int)
+            print(int + " is a leap year")
+            return True
+        else:
+            int = str(int)
+            print(int + " is not a leap year")
+            return False
 
 leapCheck(2020)
 leapCheck(2021)
+leapCheck(1900)
