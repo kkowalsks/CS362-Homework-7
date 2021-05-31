c73c9b048839704675432da07a0b637b99c49642
Kyle <kyle216@gmail.com>
2021-05-30 20:31:36 -0700

    Wrote 2nd test, checks if multiples of 100 are returned as False
    The test currently fails since 1900 is a multiple of 4, and it returns True.

--- test_leapyear.py
+++ test_leapyear.py
@@ -3,3 +3,6 @@ import leapyear
 def test_four():
     assert leapyear.leapCheck(2020) == True
     assert leapyear.leapCheck(2021) == False
+
+def test_hundred():
+    assert leapyear.leapCheck(1900) == False
